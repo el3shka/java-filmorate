f343c08716fb3caa65766f5ab820dfcd96c8ac4d
Vitaliy P. <94363715+el3shka@users.noreply.github.com>
2024-07-18 22:00:12 +0600

    Update UserTest.java

--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
