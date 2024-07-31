6d8c68404e7f9a61d1b044c264180f99a453bdf0
el3shka <harv3st3r666@gmail.com>
2024-07-31 20:51:36 +0600

    sprint-11-rebased

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
     }
 
     public List<User> findAll() {
-        return getUsers().values();
+        return (List<User>) getUsers().values();
     }
 
     public User create(User user) {
